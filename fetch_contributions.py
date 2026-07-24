d7316fd3172f15cd5fb826ac5b5b7c167c967c2d
Harshit aggarwal <92847950+HarshitAggarwal1902@users.noreply.github.com>
2026-07-24 14:45:25 +0530

    Update fetch_contributions.py

--- fetch_contributions.py
+++ fetch_contributions.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import os
 
-# IMPORTANT: REPLACE THIS WITH YOUR ACTUAL GITHUB USERNAME
-GITHUB_USERNAME = "YOUR_USERNAME_HERE"
+GITHUB_USERNAME = "HarshitAggarwal1902"
 
 def fetch_contributions(username):
     url = f"https://github.com/users/{username}/contributions"
